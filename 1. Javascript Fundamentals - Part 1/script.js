6bb3eb46a2b2495322a6774baf95a82aebdc48aa
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-01-06 23:39:57 +0530

    Values and Variables

--- 1. Javascript Fundamentals - Part 1/script.js
+++ 1. Javascript Fundamentals - Part 1/script.js
@@ -1,7 +1,43 @@
+// ================================================================================================================
 // Linking Javascript file 
-
+// ================================================================================================================
+/*
 let js = 'amazing';
 if (js === 'amazing')
     alert('javascript is fun');
 
 console.log(40 + 8 + 23 - 10);
+*/
+
+// ================================================================================================================
+// Values and variables
+// ================================================================================================================
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+// variable name can only contain letters, numbers, underscore and dollar sign
+// variable name cannot be any reserved keyword
+// variable name cannot start from number
+let firstName = "Jonas";
+let PI = 3.14;
+let jonal_matilda = "JM";
+let $function = 27;
+let _hello = "underscore_hello";
+
+console.log(firstName);
+console.log($function);
+
+// ---------------------------------------------------------------------------
+// Assignment #1
+// ---------------------------------------------------------------------------
+
+let country = "India";
+let continent = "Asia";
+let population = 9876543210;
+
+console.log(country);
+console.log(continent);
+console.log(population);
