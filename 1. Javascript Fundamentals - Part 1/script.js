0175d899bba54a05ecd8575d516a6cde7c7ac900
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-01-12 22:41:55 +0530

    Operator Precedence

--- 1. Javascript Fundamentals - Part 1/script.js
+++ 1. Javascript Fundamentals - Part 1/script.js
@@ -89,7 +89,7 @@ lastName = "Kumar";
 // ================================================================================================================
 // Basic Operators
 // ================================================================================================================
-
+/*
 // 1. Arithmetic operators
 
 const now = 2037;
@@ -120,3 +120,22 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 < now - 2018);
+*/
+
+// ================================================================================================================
+// Operator Precedence
+// ================================================================================================================
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 < now - 2018);
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+
+
+let x, y;
+x = y = 25 - 10 - 5;
+
+console.log(x, y);
