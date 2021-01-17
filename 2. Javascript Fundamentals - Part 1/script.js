4d7ef888fac4237781eb7e9590e21687f2900129
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-01-17 16:35:39 +0530

    Strict Mode

---
+++ 2. Javascript Fundamentals - Part 1/script.js
@@ -0,0 +1,17 @@
+// ================================================================================================================
+// Using strict mode
+// ================================================================================================================
+
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriverLicense = true;
+
+if(hasDriversLicense)
+console.log('I can drive!');
+
+//reserved strict keywords
+const interface = 'Audio';
+const private = 534;
