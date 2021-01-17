f8037d582c07c6e09010ad23c6426a0c207e7198
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-01-17 16:52:50 +0530

    Functions

--- 2. Javascript Fundamentals - Part 1/script.js
+++ 2. Javascript Fundamentals - Part 1/script.js
@@ -1,7 +1,7 @@
 // ================================================================================================================
 // Using strict mode
 // ================================================================================================================
-
+/*
 'use strict';
 
 let hasDriversLicense = false;
@@ -15,3 +15,47 @@ console.log('I can drive!');
 //reserved strict keywords
 const interface = 'Audio';
 const private = 534;
+*/
+
+
+// ================================================================================================================
+// Functions
+// ================================================================================================================
+
+function logger() {
+    console.log('My name is Jonas');
+}
+  
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+
+// Parameters
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+
+    //return value
+    return juice;
+}
+  
+//Arguments
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+ 
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+// Built-In Functions
+const num = Number('23');
+
+//example
+function add(value1, value2){
+    return value1 + value2;
+}
+
+const concatString = add('Hello ', 'World');
+const addNum = add(5,6);
+
+console.log(concatString, addNum);
