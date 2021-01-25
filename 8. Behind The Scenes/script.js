fbd22351dc1810c25388b2148391830bea920281
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-01-25 19:43:20 +0530

    Hoisting in Practice

--- 8. Behind The Scenes/script.js
+++ 8. Behind The Scenes/script.js
@@ -3,7 +3,7 @@
 // ================================================================================================================
 // Scoping in Practice
 // ================================================================================================================
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -40,3 +40,51 @@ const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age);
 // printAge();
+*/
+
+// ================================================================================================================
+// Hoisting in Practice
+// ================================================================================================================
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
