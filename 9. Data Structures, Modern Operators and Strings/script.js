ee67a1c55fdcd7107088ce7d037c84f4a0c95998
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-01-30 19:00:37 +0530

    Rest Pattern and Parameters

--- 9. Data Structures, Modern Operators and Strings/script.js
+++ 9. Data Structures, Modern Operators and Strings/script.js
@@ -37,6 +37,11 @@ const restaurant = {
       `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 // ================================================================================================================
@@ -128,7 +133,7 @@ restaurant.orderDelivery({
 // ================================================================================================================
 // Spread Operator (...)
 // ================================================================================================================
-
+/*
 const arr = [7, 8, 9];
 
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
@@ -171,3 +176,41 @@ const restaurantCopy = { ...restaurant };
 restaurantCopy.name = 'Ristorante Roma';
 console.log(restaurantCopy.name);
 console.log(restaurant.name);
+*/
+
+// ================================================================================================================
+// Rest Pattern and Parameters
+// ================================================================================================================
+
+// Spread, because on right side of =
+const arr = [1, 2, ...[3, 4]];
+
+// Rest, because on left side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+//Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+//functions : rest arguments
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
