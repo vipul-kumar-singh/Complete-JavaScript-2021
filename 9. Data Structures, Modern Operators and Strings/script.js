539fda084985b2213a18aae4b1fd4f0752af2e9c
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-01-30 22:27:26 +0530

    Optional Chaining

--- 9. Data Structures, Modern Operators and Strings/script.js
+++ 9. Data Structures, Modern Operators and Strings/script.js
@@ -286,7 +286,7 @@ console.log([...menu.entries()]);
 // ================================================================================================================
 // Enhanced Object Literals
 // ================================================================================================================
-
+/*
 // 1. Nested objects
 const obj1 = {
   name: 'Object 1',
@@ -331,3 +331,33 @@ const obj4 = {
 };
 
 console.log(obj4);
+*/
+
+// ================================================================================================================
+// Optional Chaining (?.)
+// ================================================================================================================
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+//Arrays
+const users = [{ name: 'Jonas', email: 'hello@jonas.io' }];
+// const users = [];
+
+console.log(users[0]?.name ?? 'User array empty');
