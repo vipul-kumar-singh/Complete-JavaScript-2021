43a66c5a7fb85d2a8ef259e6ad659eddaab4b1d6
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-06 00:31:01 +0530

    Immediately Invoked Function Expressions

--- 10. Functions/script.js
+++ 10. Functions/script.js
@@ -126,7 +126,7 @@ greetArr('Hi')('Joe');
 // ================================================================================================================
 // The call and apply methods
 // ================================================================================================================
-
+/*
 const lufthansa = {
   airline: 'Lufthansa',
   iataCode: 'LH',
@@ -222,3 +222,30 @@ const addVAT2 = addTaxRate(0.23);
 
 console.log(addVAT2(100));
 console.log(addVAT2(23));
+*/
+
+//================================================================================================================
+// Immediately Invoked Function Expressions (IIFE)
+// ================================================================================================================
+
+const runOnce = function () {
+  console.log('This will run again');
+};
+runOnce();
+
+// IIFE
+(function () {
+  console.log('This will never run again');
+  const isPrivate = 23;
+})();
+
+// console.log(isPrivate);
+
+(() => console.log('This will ALSO never run again'))();
+
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
+}
+// console.log(isPrivate);
+console.log(notPrivate);
