48d58cb207c408a57fb069e3cec3230e7b65b932
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-14 00:29:28 +0530

    The map method

--- 11. Arrays - Bankist/script.js
+++ 11. Arrays - Bankist/script.js
@@ -9,7 +9,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // ================================================================================================================
 // Simple Array Methods
@@ -78,7 +78,7 @@ movements.forEach(function (mov, i, arr) {
 // ================================================================================================================
 // forEach with Maps and Sets
 // ================================================================================================================
-
+/*
 //Map
 const currenciesMap = new Map([
   ['USD', 'United States dollar'],
@@ -96,3 +96,25 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 });
+*/
+
+// ================================================================================================================
+// The map method
+// ================================================================================================================
+
+const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function(mov) {
+//  return mov * eurToUsd;
+// });
+
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsDescription = movements.map((mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(mov)}`
+);
+
+console.log(movementsDescription);
