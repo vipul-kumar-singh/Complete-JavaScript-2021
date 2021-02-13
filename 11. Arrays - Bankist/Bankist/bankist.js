f4d1c127f540cd2a08262091bc90903573bd6ea0
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-14 00:38:15 +0530

    Computing Usernames

--- 11. Arrays - Bankist/Bankist/bankist.js
+++ 11. Arrays - Bankist/Bankist/bankist.js
@@ -78,5 +78,16 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-
 displayMovements(account1.movements);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+console.log(accounts);
