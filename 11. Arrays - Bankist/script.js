f90b858537c434727ecd0d36989535e2c0b78702
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-13 20:36:20 +0530

    Looping Arrays - forEach

--- 11. Arrays - Bankist/script.js
+++ 11. Arrays - Bankist/script.js
@@ -71,14 +71,14 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
 // ================================================================================================================
 // Simple Array Methods
 // ================================================================================================================
-
+/*
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 // 1. Slice - splits the array and returns new array
@@ -100,7 +100,7 @@ console.log(arr);
 // 3. Reverse - Reverses the array and mutates the original array
 arr = ['a', 'b', 'c', 'd', 'e'];
 const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse);
+console.log(arr2.reverse());
 console.log(arr2);
 
 // 4. Concat - adds 2 arrays and returns new array
@@ -110,3 +110,30 @@ console.log(...arr, ...arr2);
 
 // 5. Join - returns array elemtents as string joined by variable
 console.log(letters.join(' - '));
+*/
+
+// ================================================================================================================
+// Looping Arrays - forEach
+// ================================================================================================================
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// For-of
+console.log(`-------FOR-OF-------`);
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+// For-each
+console.log(`-------FOR-EACH-------`);
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
