46f14d6bf80e1d3c136cde9f88a791b1dbfb8dc8
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-17 22:55:40 +0530

    Creating Dates

--- 12. Numbers-Dates-Timers-Bankist/script.js
+++ 12. Numbers-Dates-Timers-Bankist/script.js
@@ -377,7 +377,7 @@ function paintBg() {
 // ================================================================================================================
 // Working with BigInt
 // ================================================================================================================
-
+/*
 console.log(2 ** 53 - 1);
 console.log(Number.MAX_SAFE_INTEGER);
 
@@ -408,3 +408,42 @@ console.log(20n === 20);
 
 // typeof
 console.log(typeof 20n);
+*/
+
+// ================================================================================================================
+// Creating Dates
+// ================================================================================================================
+
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Aug 02 2020 18:05:41'));
+console.log(new Date('December 24, 2015'));
+
+console.log(new Date(account1.movementsDates[0]));
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+console.log(new Date(2037, 10, 33));
+
+//millis
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+//Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+
+console.log(future.getTime()); // convert date as millis
+console.log(new Date(2142237180000)); // convert millis to date
+
+// get current millis
+console.log(Date.now());
