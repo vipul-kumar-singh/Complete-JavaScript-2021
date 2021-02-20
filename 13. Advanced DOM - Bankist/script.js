b732604542481b3e449642ca85a1671a5f64c44b
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-20 23:33:49 +0530

    Styles, Attributes and Classes

--- 13. Advanced DOM - Bankist/script.js
+++ 13. Advanced DOM - Bankist/script.js
@@ -56,8 +56,8 @@ message.classList.add('cookie-message');
 message.innerHTML =
   'We use cookies for improved functionality and analytics. <button class = "btn btn--close-cookie">Got it!</button>';
 
-// header.prepend(message);
-header.append(message);
+header.prepend(message);
+// header.append(message);
 // header.append(message.cloneNode(true));
 
 // header.before(message);
@@ -70,3 +70,55 @@ document
     // message.remove(); // new
     message.parentElement.removeChild(message); // old
   });
+
+// ================================================================================================================
+// Styles, Attributes and Classes
+// ================================================================================================================
+
+//Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color); // will not work because it is set in css file
+console.log(message.style.backgroundColor); // will work because it is set in html file
+
+console.log(getComputedStyle(message).color); // will work
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard Attributes
+console.log(logo.designer); // will not work for custom attributes
+console.log(logo.getAttribute('designer')); // will work
+
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.src); // Absolute value
+console.log(logo.getAttribute('src')); // Relative value
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data Attributes : prefix - data-
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+// Don't use because it will override all the classes
+// logo.className = 'jonas';
