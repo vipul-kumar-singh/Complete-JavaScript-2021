638c8dd60eb4638189a761283733c818dbcb26cd
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-28 02:07:45 +0530

    Lazy Loading images

--- 13. Advanced DOM - Bankist/script.js
+++ 13. Advanced DOM - Bankist/script.js
@@ -162,6 +162,31 @@ allSections.forEach(function (section) {
   section.classList.add('section--hidden');
 });
 
+// Lazy Loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '+200px',
+});
+imgTargets.forEach(img => imgObserver.observe(img));
+
 // ================================================================================================================
 // Selecting, Creating and Deleting Elements
 // ================================================================================================================
