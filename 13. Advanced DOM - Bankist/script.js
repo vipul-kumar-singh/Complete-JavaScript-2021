939bc5b1534654b2e9f7a3c3cc52937350f726fd
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-28 00:36:29 +0530

    Tabbed Component

--- 13. Advanced DOM - Bankist/script.js
+++ 13. Advanced DOM - Bankist/script.js
@@ -79,6 +79,30 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+// Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard Clause
+  if (!clicked) return;
+
+  // Remove Active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(tc => tc.classList.remove('operations__content--active'));
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  // Active content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
 // ================================================================================================================
 // Selecting, Creating and Deleting Elements
 // ================================================================================================================
@@ -271,7 +295,7 @@ document.querySelector('.nav').addEventListener(
 // ================================================================================================================
 // DOM Traversing
 // ================================================================================================================
-
+/*
 const h1 = document.querySelector('h1');
 
 // Going downwards: child
@@ -299,3 +323,4 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = 'scale(0.5)';
 });
+*/
