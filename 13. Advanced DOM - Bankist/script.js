e8fdb7b05769a6c6db3f121397b4333f3fd5fda7
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-28 00:19:23 +0530

    DOM Traversing

--- 13. Advanced DOM - Bankist/script.js
+++ 13. Advanced DOM - Bankist/script.js
@@ -267,3 +267,35 @@ document.querySelector('.nav').addEventListener(
   // ,true // capturing
 );
 */
+
+// ================================================================================================================
+// DOM Traversing
+// ================================================================================================================
+
+const h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes); // Nodes List
+console.log(h1.children); // HTML Collection (Only for direct child)
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways : siblings (only direct siblings i.e., previous and next)
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
