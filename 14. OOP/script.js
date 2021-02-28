43276ef48656f00efa37fc44acbb9db66ebbd7e2
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-28 21:09:28 +0530

    Getters and Setters

--- 14. OOP/script.js
+++ 14. OOP/script.js
@@ -93,8 +93,8 @@ console.dir(x => x + 1);
 
 // class declaration
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
 
@@ -106,9 +106,23 @@ class PersonCl {
   greet() {
     console.log(`Hey ${this.firstName}`);
   }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcage();
 
@@ -119,3 +133,32 @@ console.log(jessica.__proto__ === PersonCl.prototype);
 // }
 
 jessica.greet();
+
+console.log(jessica.age);
+
+const walter = new PersonCl('Walter White', 1965);
+
+// ================================================================================================================
+// Getters and Setters
+// ================================================================================================================
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  // returns a value
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  // requires exactly 1 parameter
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+// Called as a property
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
