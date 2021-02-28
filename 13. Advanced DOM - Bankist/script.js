3416ee919d24106f2e53879212a5d207bd5e1511
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-28 14:39:59 +0530

    Lifecycle DOM Events

--- 13. Advanced DOM - Bankist/script.js
+++ 13. Advanced DOM - Bankist/script.js
@@ -528,3 +528,21 @@ const obsOptions = {
 const observer = new IntersectionObserver(obsCallback, obsOptions);
 observer.observe(section1);
 */
+
+// ================================================================================================================
+// Lifecycle DOM Events
+// ================================================================================================================
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  console.log('HTML parsed and DOM tree build!', e);
+});
+
+window.addEventListener('load', function (e) {
+  console.log('Page fully loaded!', e);
+});
+
+window.addEventListener('beforeunload', function (e) {
+  e.preventDefault();
+  console.log(e);
+  e.returnValue = '';
+});
