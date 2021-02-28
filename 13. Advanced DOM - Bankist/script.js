72ae13fd3293a31770cbc8ead8deb25b3302c7f5
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-28 14:09:01 +0530

    Building a Slider Component - Part 1

--- 13. Advanced DOM - Bankist/script.js
+++ 13. Advanced DOM - Bankist/script.js
@@ -159,7 +159,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  // section.classList.add('section--hidden');
 });
 
 // Lazy Loading images
@@ -186,6 +186,51 @@ const imgObserver = new IntersectionObserver(loadImg, {
   rootMargin: '+200px',
 });
 imgTargets.forEach(img => imgObserver.observe(img));
+
+// Building a Slider Component
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+// const slider = document.querySelector('.slider');
+// slider.style.transform = 'scale(0.4) translateX(-800px)';
+// slider.style.overflow = 'visible';
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+// Next Slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  goToSlide(curSlide);
+};
+
+// Prev Slide
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
 
 // ================================================================================================================
 // Selecting, Creating and Deleting Elements
