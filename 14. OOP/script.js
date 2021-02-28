c183e1d3d3f9e15aadb39b6f53e9c274be2ddaaa
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-28 23:12:09 +0530

    Another Class Example

--- 14. OOP/script.js
+++ 14. OOP/script.js
@@ -327,7 +327,7 @@ martha.calcage();
 // ================================================================================================================
 // Inheritance Between Classes: Object.create
 // ================================================================================================================
-
+/*
 const PersonProto = {
   calcage() {
     console.log(2037 - this.birthYear);
@@ -355,3 +355,51 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Information Technology');
 jay.introduce();
 jay.calcage();
+*/
+
+// ================================================================================================================
+// Another Class Example
+// ================================================================================================================
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}.`);
+  }
+
+  // Public Interfaces
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) this.deposit(val);
+    console.log('Loan approved');
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+console.log(acc1);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
