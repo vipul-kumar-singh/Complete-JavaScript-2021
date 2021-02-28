46fa58683bfa2e75d8bd0d62a6bc84d3b733087e
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-28 22:35:57 +0530

    Inheritance Between Classes: ES6 Classes

--- 14. OOP/script.js
+++ 14. OOP/script.js
@@ -214,7 +214,7 @@ sarah.calcage();
 // ================================================================================================================
 // Inheritance Between Classes: Constructor Functions
 // ================================================================================================================
-
+/*
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -252,3 +252,72 @@ console.log(mike instanceof Object);
 console.dir(Student.prototype.constructor);
 Student.prototype.constructor = Student;
 console.dir(Student.prototype.constructor);
+*/
+
+// ================================================================================================================
+// Inheritance Between Classes: ES6 Classes
+// ================================================================================================================
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Instance Methods
+  calcage() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static method
+  static hey() {
+    console.log('Hey 👋');
+  }
+}
+
+// extends links prototype behind the scenes
+class StudentCl extends PersonCl {
+  constructor(fullName, birthYear, course) {
+    // Always needs to happen first!
+    super(fullName, birthYear);
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}.`);
+  }
+
+  // Override method
+  calcage() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }.`
+    );
+  }
+}
+
+const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+console.log(martha);
+
+martha.introduce();
+martha.calcage();
