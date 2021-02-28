9bc27330e1257b03de305173cc5a4ed9d8558790
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-28 19:21:06 +0530

    Prototypes

--- 14. OOP/script.js
+++ 14. OOP/script.js
@@ -28,3 +28,28 @@ const jack = new Person('Jack', 2017);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person);
+
+// ================================================================================================================
+// Prototypes
+// ================================================================================================================
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
