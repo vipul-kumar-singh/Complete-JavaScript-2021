d23c3e934beb0cfaeeafd49cae632eec02231386
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-02-28 23:16:56 +0530

    Encapsulation: Protected Properties and Methods

--- 14. OOP/script.js
+++ 14. OOP/script.js
@@ -365,28 +365,35 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
+
+    //protected property
     this.pin = pin;
-    this.movements = [];
+    this._movements = [];
+
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}.`);
   }
 
   // Public Interfaces
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) this.deposit(val);
+    if (this._approveLoan(val)) this.deposit(val);
     console.log('Loan approved');
   }
 }
@@ -394,12 +401,20 @@ class Account {
 const acc1 = new Account('Jonas', 'EUR', 1111);
 console.log(acc1);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+
+//will work
+acc1._approveLoan(1000);
 
 console.log(acc1);
 console.log(acc1.pin);
+
+// ================================================================================================================
+// Encapsulation: Protected Properties and Methods
+// ================================================================================================================
+
+console.log(acc1.getMovements());
