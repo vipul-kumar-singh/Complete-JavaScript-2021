9da2befb9d634cf2bdce1d5d6f72b2aeaba4e663
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-03-01 23:04:11 +0530

    Displaying a Map Using Leaflet Library

--- 15. Mapty/script.js
+++ 15. Mapty/script.js
@@ -19,6 +19,21 @@ if (navigator.geolocation)
       console.log(
         `https://www.google.co.in/maps/@${latitude},${longitude},20z`
       );
+
+      // Leaflet
+      const coords = [latitude, longitude];
+
+      var map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position');
