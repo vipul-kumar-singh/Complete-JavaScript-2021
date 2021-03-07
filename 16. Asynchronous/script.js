0e0199f89b2332e4aaf4a461d9af94dd293c1272
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-03-07 19:47:51 +0530

    Running Promises in Parallel

--- 16. Asynchronous/script.js
+++ 16. Asynchronous/script.js
@@ -359,7 +359,7 @@ btn.addEventListener('click', whereAmI);
 // ================================================================================================================
 // Consuming Promises with Async/Await
 // ================================================================================================================
-
+/*
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -421,7 +421,7 @@ try {
 // ================================================================================================================
 // Returning values from Async Functions
 // ================================================================================================================
-
+/*
 console.log('1: Will get location');
 // const city = whereAmI();
 // console.log(city);
@@ -440,3 +440,36 @@ console.log('1: Will get location');
   }
   console.log('3: Finished getting location');
 })();
+*/
+
+// ================================================================================================================
+// Running Promises in Parallel
+// ================================================================================================================
+
+const getJSON = function (url, errorMsg = 'Something went wrong!') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.eu/rest/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.eu/rest/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.eu/rest/v2/name/${c3}`);
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.eu/rest/v2/name/${c1}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c2}`),
+      getJSON(`https://restcountries.eu/rest/v2/name/${c3}`),
+    ]); // If one promise rejects, all promise rejects
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
