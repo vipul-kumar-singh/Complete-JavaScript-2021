f80ff6d5f1657202ca67d22a4945d42e8a03ad4b
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-03-07 20:34:00 +0530

    Other Promise Combinators: race, allSettled and any

--- 16. Asynchronous/script.js
+++ 16. Asynchronous/script.js
@@ -453,7 +453,7 @@ const getJSON = function (url, errorMsg = 'Something went wrong!') {
     return response.json();
   });
 };
-
+/*
 const get3Countries = async function (c1, c2, c3) {
   try {
     // const [data1] = await getJSON(`https://restcountries.eu/rest/v2/name/${c1}`);
@@ -473,3 +473,57 @@ const get3Countries = async function (c1, c2, c3) {
 };
 
 get3Countries('portugal', 'canada', 'tanzania');
+*/
+
+// ================================================================================================================
+// Other Promise Combinators: race, allSettled and any
+// ================================================================================================================
+
+// Promise.race - Returns first settled promise
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.eu/rest/v2/name/italy`),
+    getJSON(`https://restcountries.eu/rest/v2/name/egypt`),
+    getJSON(`https://restcountries.eu/rest/v2/name/mexico`),
+  ]);
+  console.log(res[0]);
+})();
+
+const timeout = function (sec) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error('request took too long!'));
+    }, sec * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.eu/rest/v2/name/tanzania`),
+  timeout(0.001),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+// Promise.allSettled - Never rejects and returns array of all settled promise
+Promise.allSettled([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another resolve'),
+]).then(res => console.log(res));
+
+Promise.all([
+  Promise.resolve('Success'),
+  Promise.reject('Error'),
+  Promise.resolve('Another resolve'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
+
+// Promise.any [ES2021] - Returns first fullfilled promise and ignores rejected promise until all rejects
+Promise.allSettled([
+  Promise.resolve('Success Any'),
+  Promise.reject('Error Any'),
+  Promise.resolve('Another resolve Any'),
+])
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
