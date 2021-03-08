c9a2a8a259718ab369a6056fa38a80455c827c51
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-03-09 00:02:02 +0530

    Module Pattern

--- 17. Modern JS, Modules, Tooling/script.js
+++ 17. Modern JS, Modules, Tooling/script.js
@@ -1,7 +1,7 @@
 // ================================================================================================================
 // Exporting and Importing in ES6 modules
 // ================================================================================================================
-
+/*
 //// Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js'; //named import
 // console.log(shippingCost // will not work);
@@ -27,3 +27,38 @@ add('pizza', 2);
 add('bread', 5);
 add('apples', 4);
 console.log(cart);
+*/
+
+// ================================================================================================================
+// Module Pattern (Not used in modern development)
+// ================================================================================================================
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantiy} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
