3ad8d1a8bb6bb11c8bd92dae756e0b5b93a74a01
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-03-16 22:54:55 +0530

    Updating Recipe Servings

--- 18. Forkify/src/js/controller.js
+++ 18. Forkify/src/js/controller.js
@@ -60,8 +60,15 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+  recipeView.render(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
--- 18. Forkify/src/js/model.js
+++ 18. Forkify/src/js/model.js
@@ -62,3 +62,12 @@ export const getSearchResultsPage = function (page = state.search.page) {
   const end = page * state.search.resultsPerPage;
   return state.search.results.slice(start, end);
 };
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ing => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+    // newQt = oldQt * newServings / oldServings
+  });
+
+  state.recipe.servings = newServings;
+};
