3bd49ce2aba1b6502fb44d324db3dbef795c7a2e
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-03-17 22:01:59 +0530

    Developing a DOM Updating Algorithm

--- 18. Forkify/src/js/controller.js
+++ 18. Forkify/src/js/controller.js
@@ -21,6 +21,9 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    // 0. Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
     // 1. Loading Recipe
     await model.loadRecipe(id);
 
@@ -63,7 +66,8 @@ const controlPagination = function (goToPage) {
 const controlServings = function (newServings) {
   // Update the recipe servings (in state)
   model.updateServings(newServings);
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
