d246332fd3d0a10b5450254182a9cad12b43d96d
vipul-kumar-singh <vipul24.9669@gmail.com>
2021-03-18 18:11:25 +0530

    Uploading a New Recipe - Part 1

--- 18. Forkify/src/js/controller.js
+++ 18. Forkify/src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -94,6 +95,12 @@ const clearBookmarks = function () {
 };
 // clearBookmarks();
 
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+
+  //Uplaod the new recipe data
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -101,5 +108,6 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
